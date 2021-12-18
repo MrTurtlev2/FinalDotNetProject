5c066959850454c671abc6d9d9b4762ab06f90f0
Hubert <trebuh@MacBook-Pro-Hubert-2.local>
2021-12-18 13:07:39 +0100

    Restore seeding

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using FinalDotNetProject.Models;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace FinalDotNetProject
 {
@@ -13,7 +15,24 @@ namespace FinalDotNetProject
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedDatabase.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
